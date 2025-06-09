08991b8a5ea5705410b2005b01801dae129d6440
Vitor Fernandes <vfgi@hotmail.com>
2025-06-09 21:17:40 +0100

    add the tokens to tailwind config on next

--- Next/src/providers/providers.tsx
+++ Next/src/providers/providers.tsx
@@ -1,8 +1,15 @@
 'use client';
 
 import { Provider } from 'jotai';
+
+import { DesignTokenProvider } from '@/components/DesignTokenProvider';
+
 import '@/i18n';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider>{children}</Provider>;
+  return (
+    <Provider>
+      <DesignTokenProvider>{children}</DesignTokenProvider>
+    </Provider>
+  );
 }
